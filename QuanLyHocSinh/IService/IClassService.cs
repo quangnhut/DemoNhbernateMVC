76669b1bcbc3bf44f4e92a616994590edbf0d8b2
quang nhut <quangnhutcntt@gmail.com>
2019-12-27 10:14:00 +0700

    new commit

--- QuanLyHocSinh/IService/IClassService.cs
+++ QuanLyHocSinh/IService/IClassService.cs
@@ -20,14 +20,10 @@ namespace QuanLyHocSinh.IService
         void InsertStudent(Student student);
         List<Class> SearchByName(string name);
         List<Student> GetStudentByClassId(int id);
-        //develop commpent
-        //develop commpent
-        //develop commpent
-        //develop commpent
-        //develop commpent
-        //develop commpent
-        //develop commpent
 
+
+        //tester comment
+        //tester comment
         //tester comment
         //tester comment
         //tester comment
